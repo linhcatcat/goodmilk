ba91e0037d07a97ea6a4644bc332f121110b8022
Alex <alex@likipe.se>
2015-01-16 15:57:52 +0700

    list news page

--- sites/all/modules/my_utilities/templates/news-cate.tpl.php
+++ sites/all/modules/my_utilities/templates/news-cate.tpl.php
@@ -1,6 +1,19 @@
 <?php
 	global $base_url, $user;
 	$taxo = taxonomy_get_tree(1);
+	$pager_limit = 3;
+	$sql = "SELECT node.nid AS nid,
+	   		node.title AS title,
+	   		node_data_field_news_content.field_news_content_value AS news_content,
+	   		node_data_field_news_content.field_news_images_fid AS news_images_fid,
+	   		node_data_field_news_content.field_news_summary_value AS news_summary,
+	   		node.changed AS node_changed
+	 	FROM node node
+	 	INNER JOIN term_node term_node ON node.vid = term_node.vid
+	 	LEFT JOIN content_type_news node_data_field_news_content ON node.vid = node_data_field_news_content.vid
+	 	WHERE (node.status <> 0) AND (node.type in ('news')) AND (term_node.tid = ". $tid .")
+	   	ORDER BY node_changed DESC";
+	$obj = pager_query($sql,$pager_limit, 0, NULL);
 ?>
 <div class="row-fluid">
 	<div class="span12">
@@ -11,14 +24,25 @@
 		</ul>
 	</div>
 </div>
-<div class="row-fluid">
+<?php while($obj_rs = db_fetch_object($obj)){ ?>
+	<?php
+		$arr = field_file_load($obj_rs->news_images_fid);
+		$path = $arr['filepath'];
+		$url = drupal_get_path_alias('node/'. $obj_rs->nid);
+	?>
+<div class="row-fluid news-item">
 	<div class="span3 thumb">
-		<a href="#"><img src="<?php echo $base_url .'/'. drupal_get_path('theme', 'Bootstrap').'/images/product-thumb.jpg'; ?>"/></a>
+		<?php if( $obj_rs->news_images_fid ){ ?>
+		<a href="<?php print  $base_url .'/'. $url; ?>"><img src="<?php echo $base_url .'/'. $path; ?>"/></a>
+		<?php } ?>
 	</div>
 	<div class="span9 text">
-		<p class="title"><a href="#">Những sai lầm của mẹ Việt về dinh dưỡng cho trẻ</a></p>
-		<p class="date">Update: 20/05/2014 - 14:23</p>
-		<div class="summary">Theo bác sĩ Nguyễn Thị Hoa (Nguyên trưởng khoa Dinh dưỡng Bệnh viện Nhi đồng 1, TP HCM), sai lầm thường gặp ở các bà mẹ là quan niệm sai lệch về cân nặng. Với các mẹ, trẻ mũm mĩm đồng nghĩa với khỏe mạnh.</div>
+		<p class="title"><?php print l($obj_rs->title, $url); ?></p>
+		<p class="date">Update: <?php print date('d/m/Y - h:i', $obj_rs->node_changed); ?></p>
+		<div class="summary"><?php print $obj_rs->news_summary; ?></div>
 	</div>
 	<div class="clearfix"></div>
 </div>
+<?php } ?>
+
+<?php print theme('pager', NULL, $pager_limit, 0); ?>
