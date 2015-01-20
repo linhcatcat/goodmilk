faec8792f7a91e7d445499a48eb6af69c561d236
Alex <alex@likipe.se>
2015-01-20 13:40:16 +0700

    update slide show

--- sites/all/modules/my_utilities/templates/slideshow1.tpl.php
+++ sites/all/modules/my_utilities/templates/slideshow1.tpl.php
@@ -4,6 +4,13 @@
 		'type' => 'slideshow1',
 	));
 ?>
+<div class="slider fade-slide-show">
+	<?php foreach ($slideshow->field_slideshow1_image as $key => $item) {?>
+		<div><div class="image"><img src="<?php echo $base_url .'/'. $item['filepath']; ?>"/></div></div>
+	<?php } ?>
+</div>
+
+<?php if(0){ ?>
 <div id="myCarousel" class="carousel slide carousel-fade">
 	<!-- Carousel items -->
 	<div class="carousel-inner">
@@ -12,3 +19,17 @@
 		<?php } ?>
 	</div>
 </div>
+<?php } ?>
+<script>
+	$('.fade-slide-show').slick({
+		dots: false,
+		infinite: true,
+		speed: 2000,
+		fade: true,
+		slide: 'div',
+		cssEase: 'linear',
+		autoplay: true,
+		prevArrow: '',
+		nextArrow: ''
+	});
+</script>
